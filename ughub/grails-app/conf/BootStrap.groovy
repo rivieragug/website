5fe55da6700faa2036271b4b402307031b4652d4
Frédéric Guillaume <guillaume.frederic@gmail.com>
2012-08-11 04:47:25 +0200

    Bootstrap update for creating entries withing the DB

--- ughub/grails-app/conf/BootStrap.groovy
+++ ughub/grails-app/conf/BootStrap.groovy
@@ -1,9 +1,11 @@
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
-    	if (Environment.current == Environment.DEV){
+    	if (Environment.current == Environment.DEVELOPMENT){
     		if (!org.ughub.Group.count()) {
-            new org.ughub.Group(name: "Coding Week-End Group", description: "Pizzas & Beer are legions", logoPath: "/tmp").save(failOnError: true)
+            new org.ughub.Group(name: "Coding Week-End Group", description: "Pizzas & Beers are legions", logoPath: "/tmp").save(failOnError: true)
 	        }
 	        if (!org.ughub.Member.count()){
 	    		new org.ughub.Member(firstName: "Admin", lastName: "Admin", userName: "admin").save(failOnError: true)
