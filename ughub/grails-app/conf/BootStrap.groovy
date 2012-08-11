5e82b00e70933a05c0dbf1f9093fbd37f46d83ea
Filippo Pacifici <filippo.pacifici@gmail.com>
2012-08-11 05:43:06 +0200

    Added roles creation

--- ughub/grails-app/conf/BootStrap.groovy
+++ ughub/grails-app/conf/BootStrap.groovy
@@ -1,3 +1,5 @@
+import org.ughub.UserAuthority;
+
 import grails.util.Environment
 
 class BootStrap {
@@ -10,13 +12,30 @@ class BootStrap {
 	        if (!org.ughub.Member.count()){
 	    		new org.ughub.Member(firstName: "Admin", lastName: "Admin", userName: "admin").save(failOnError: true)
 	    	}
-    		if (!org.ughub.User.count()){
-				new org.ughub.User(username: 'admin', password: 'admin',enabled: true).save(failOnError: true)
-			}
+    		setUpUsers()
 		}
 		
 		
     }
+	
+	/**
+	 * Creates the admin user, the roles and assigns them
+	 */
+	def setUpUsers = {
+		def adminUser = org.ughub.User.findByUsername('admin')
+		if (adminUser == null){
+			adminUser = new org.ughub.User(username: 'admin', password: 'admin',enabled: true).save(failOnError: true)
+		}
+		
+		for (r in ['ADMIN','BASE_USER','POWER_USER']) {
+			def myRole = org.ughub.Authority.findByAuthority(r)
+			if (myRole == null){
+				myRole = new org.ughub.Authority(authority: r).save(failOnError: true)
+				new UserAuthority(authority: myRole, user : adminUser).save(failOnError : true)
+			}
+		}
+	}
+	
     def destroy = {
     }
 }
