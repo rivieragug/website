dd72653f6374e66f9c7bbe16febee78e7ba1a57b
fabricematrat <corinnekrych@gmail.com>
2012-08-11 17:13:41 +0200

    sponsor

--- ughub/grails-app/domain/org/ughub/Group.groovy
+++ ughub/grails-app/domain/org/ughub/Group.groovy
@@ -4,11 +4,13 @@ class Group {
 	String name
 	String description
 	String logoPath
-	static hasMany = [members: Member, events: Event]
+	static hasMany = [members: Member, events: Event, sponsors: Sponsor]
 	static constraints = {
 	}
 
 	static mapping = {
         collection "ugroup"
+		//sponsors(nullable:true)
     }
+	
 }
